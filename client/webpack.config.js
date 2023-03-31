977286ed675b6921c65f1b233e94591dd51228fb
box-o-water <boxooowater@gmail.com>
2023-03-31 17:10:18 -0500

    fix(webpack.config.js): trying a friends config to see if icon will finally work in heroku

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,33 +3,38 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: "development",
     entry: {
       main: "./src/js/index.js",
       install: "./src/js/install.js",
+      database: "./src/js/database.js",
+      editor: "./src/js/editor.js",
+      header: "./src/js/header.js",
     },
     output: {
       filename: "[name].bundle.js",
       path: path.resolve(__dirname, "dist"),
     },
     plugins: [
+      // Webpack plugin that generates our html file and injects our bundles
       new HtmlWebpackPlugin({
         template: "./index.html",
-        title: "text_editor",
+        title: "JATE",
       }),
+      // Injects our custom servie worker
       new InjectManifest({
         swSrc: "./src-sw.js",
         swDest: "src-sw.js",
       }),
+      // Creates a manifest.json file.
       new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
         name: "Just Another Text Editor",
-        short_name: "J.A.T.E",
-        description: "Take notes with JavaScript syntax highlighting!",
+        short_name: "JATE",
+        description: "Just another text editor",
         background_color: "#225ca3",
         theme_color: "#225ca3",
         start_url: "/",
@@ -45,6 +50,7 @@ module.exports = () => {
     ],
 
     module: {
+      // CSS Loaders
       rules: [
         {
           test: /\.css$/i,
@@ -53,6 +59,7 @@ module.exports = () => {
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
           use: {
             loader: "babel-loader",
             options: {
