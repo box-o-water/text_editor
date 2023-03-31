de9f9307eefd90e35ad4f993f68fbe3fea99da75
box-o-water <boxooowater@gmail.com>
2023-03-31 17:14:49 -0500

    fix(webpack.config.js): reverting back to mine with some more entry points

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,6 +3,9 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
 module.exports = () => {
   return {
     mode: "development",
@@ -18,23 +21,18 @@ module.exports = () => {
       path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-      // Webpack plugin that generates our html file and injects our bundles
       new HtmlWebpackPlugin({
         template: "./index.html",
-        title: "JATE",
+        title: "text_editor",
       }),
-      // Injects our custom servie worker
       new InjectManifest({
         swSrc: "./src-sw.js",
         swDest: "src-sw.js",
       }),
-      // Creates a manifest.json file.
       new WebpackPwaManifest({
-        fingerprints: false,
-        inject: true,
         name: "Just Another Text Editor",
-        short_name: "JATE",
-        description: "Just another text editor",
+        short_name: "J.A.T.E",
+        description: "Take notes with JavaScript syntax highlighting!",
         background_color: "#225ca3",
         theme_color: "#225ca3",
         start_url: "/",
@@ -50,7 +48,6 @@ module.exports = () => {
     ],
 
     module: {
-      // CSS Loaders
       rules: [
         {
           test: /\.css$/i,
@@ -59,7 +56,6 @@ module.exports = () => {
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
-          // We use babel-loader in order to use ES6.
           use: {
             loader: "babel-loader",
             options: {
